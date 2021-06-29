4098607ec97d00e0362a5d9f0e7679ef4a6723bf
David Hadas <davidh@il.ibm.com>
2021-06-29 10:16:42 +0300

    testing pipeline

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-gunicorn --chdir Guard Guard:app -w 1 -b 0.0.0.0:8080  --worker-class=gevent --worker-connections=1000
+#gunicorn --chdir Guard Guard:app -w 1 -b 0.0.0.0:8080  --worker-class=gevent --worker-connections=1000
+gunicorn   --chdir Guard Main:app -w 1 -b 0.0.0.0:8080  --worker-class=gevent --worker-connections=1000
 #--threads=2 --worker-class=gthread
