d8fea19bf13f9d53e79634019ce2c1519d8f6a21
David Hadas <davidh@il.ibm.com>
2021-07-16 18:51:58 +0300

    learnUntil, unlearnUntil, unblockUntil moved to spec

--- Guard/Main.py
+++ Guard/Main.py
@@ -7,43 +7,39 @@ import sys
 
 print("*** Guardian starts ***", flush=True)
 
-
 def evaluate(serviceId, collectorId, triggerInstance, data):
     print ("evaluate ", serviceId, collectorId, triggerInstance, data)
     evaluator.evaluate(serviceId, collectorId, triggerInstance, data)
     return random.randint(0,1) == 1
 
-
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
-
-@app.route('/reset/<serviceid>/<gateid>')
-def resetServiceGate(serviceid, gateid):
-    print ("/resetServiceGate called")
-    evaluator.resetServiceGate(serviceid, gateid)
-    return jsonify({"Reset": "OK"})
-
-@app.route('/display/')
+@app.get('/display/')
 def display():
-    print("/display called")
+    print("** /display called")
     d = evaluator.display()
     return jsonify(d)
 
-@app.route('/display/<serviceid>')
+@app.get('/display/<serviceid>')
 def displayService(serviceid):
-    print("/displayService called")
+    print("**  /displayService called")
     d = evaluator.displayService(serviceid)
     return jsonify(d)
 
 
-@app.route('/display/<serviceid>/<gateid>')
+@app.get('/display/<serviceid>/<gateid>')
 def displayServiceGate(serviceid, gateid):
-    print ("/displayServiceGate called")
+    print ("** /displayServiceGate called")
     d = evaluator.displayServiceGate(serviceid, gateid)
     return jsonify(d)
 
+@app.get('/reset/<serviceid>/<gateid>')
+def resetServiceGate(serviceid, gateid):
+    print("** /resetServiceGate called")
+    evaluator.resetServiceGate(serviceid, gateid)
+    return jsonify({"Reset": "OK"})
 
 @app.route('/eval', methods=['POST'])
 def eval():
